7818f5a8a65a674302578d623abffceef9914959
Priyanka-ch-dev <Priyankachannappahadapad@gmail.com>
2026-04-22 10:30:24 +0530

    Add warranty field

--- products/models.py
+++ products/models.py
@@ -49,6 +49,7 @@ class Product(models.Model):
     special_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     stock = models.IntegerField(default=0)
     is_active = models.BooleanField(default=True)
+    warranty = models.CharField(max_length=255, null=True, blank=True)
     view_count = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
